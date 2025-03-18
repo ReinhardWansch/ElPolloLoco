97e885cae32170b7234099092a1f106bb77ffb1e
Reinhard Wansch <reinhard@wansch.at>
2025-03-18 07:11:34 +0100

    draw objects when img loaded (decoded), add img-folder

--- js/game.js
+++ js/game.js
@@ -1,4 +1,19 @@
-let canvas= document.getElementById("canvasElem");
-let ctx= canvas.getContext("2d");
+let canvas = document.getElementById("canvasElem");
+let ctx = canvas.getContext("2d");
 
-console.log(ctx); ///DEBUG
+
+
+
+/*##########*/
+/*## DRAW ##*/
+/*##########*/
+
+function drawObjects(objects) {
+    objects.forEach((object) => {
+        drawObject(object);
+    });
+}
+
+function drawObject(object) {
+    object.draw(ctx);
+}
--- js/models/DrawableObject.js
+++ js/models/DrawableObject.js
@@ -1,3 +1,20 @@
 class DrawableObject {
-    
+    img;
+    x = 0;
+    y = 0;
+    width;
+    height;
+
+    constructor(imgPath, height = 100, width = 100) {
+        this.img = new Image();
+        this.img.src = imgPath;
+        this.height = height;
+        this.width = width;
+    }
+
+    draw(ctx) {
+        this.img.decode().then(() => {
+            ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        });
+    }
 }
