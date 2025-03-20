4d435d262f9c37745b48c9724605109ada3131f6
Reinhard Wansch <reinhard@wansch.at>
2025-03-20 04:15:31 +0100

    flipHorizontaly

--- js/game.js
+++ js/game.js
@@ -1,20 +1,21 @@
 let canvas = document.getElementById("canvasElem");
 let ctx = canvas.getContext("2d");
 
-let testObject = new DrawableObject("./img/4_enemie_boss_chicken/3_attack/G16.png", 100, 100);
-draw();
-
-
+let testObject = new DrawableObject("./img/4_enemie_boss_chicken/3_attack/G16.png", 200, 200);
+let testObject2 = new DrawableObject("./spielwiese/mauer-720x480.jpg", 720, 480);
+// draw();
+testObject.x = 250;
+testObject.y = 130;
 
 /*##########*/
 /*## DRAW ##*/
 /*##########*/
 
 function draw() {
-    console.log('draw()'); ///DEBUG
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    // console.log('draw()'); ///DEBUG
+    // clearCanvas(ctx);
     drawObject(testObject);
-    requestAnimationFrame(draw);
+    // requestAnimationFrame(draw);
 }
 
 function drawObjects(objects) {
@@ -27,7 +28,17 @@ function drawObject(object) {
     object.draw(ctx);
 }
 
+function clearCanvas(ctx) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 
 /*###########*/
 /*## DEBUG ##*/
 /*###########*/
+
+function tuEs() {
+    drawObject(testObject);
+    testObject.flipHorizontally();
+    drawObject(testObject);
+}
--- js/models/DrawableObject.js
+++ js/models/DrawableObject.js
@@ -5,17 +5,34 @@ class DrawableObject {
     width;
     height;
     ratio;
+    isFlippedHorizontally= false;
 
-    constructor(imgPath, height = 100, width = 100) {
+    constructor(imgPath, width= 100, height= 100) {
         this.img = new Image();
         this.img.src = imgPath;
-        this.height = height;
         this.width = width;
+        this.height = height;
         this.ratio = this.width / this.height;
     }
 
     draw(ctx) {
+        if (this.isFlippedHorizontally) {
+            this.drawFlippedHorizontally(ctx);
+        } else {
+            this.drawNormal(ctx);
+        }
+    }
+
+    drawNormal(ctx) {
         ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+    }
+
+    drawFlippedHorizontally(ctx) {
+        ctx.save();
+        ctx.scale(-1, 1);
+        ctx.translate(-canvas.width, 0);
+        ctx.drawImage(this.img, this.x, this.y, this.width, this.height);
+        ctx.restore();
     }
 
     scale(factor) {
@@ -31,5 +48,9 @@ class DrawableObject {
     scaleToWidth(width) {
         this.width = width;
         this.height = this.width / this.ratio;
+    }
+
+    flipHorizontally(ctx) {
+        this.isFlippedHorizontally = !this.isFlippedHorizontally;
     }
 }
