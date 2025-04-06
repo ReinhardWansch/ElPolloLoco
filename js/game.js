3694301572f00353bdbf7e85445bdbeb1ea3d3ef
Reinhard Wansch <reinhard@wansch.at>
2025-04-06 14:43:13 +0200

    startscreen works

--- js/game.js
+++ js/game.js
@@ -7,10 +7,32 @@ let canvas = document.getElementById("canvasElem");
 let ctx = canvas.getContext("2d");
 let world = new World(ctx);
 
+async function init() {
+    await world.loadLevel('./game/level1.json');
+    await world.loadCharacter('./game/pepe.json');
+    await world.loadEnemies();
+    await world.loadBottleTemplate('./game/bottle.json');
+    await world.loadEndboss('./game/polloLoco.json');
+    hideElement('loadingCtn');
+    document.getElementById('buttonStartGame').disabled= false;
+}
 
-function start() {
+function startGame() {
+    hideElement('startScreenCtn');
     world.draw(ctx);
-    world.applyGravity();
+    world.applyGravity(world.gravity);
+    startEnemyMovement();
+}
+
+function startEnemyMovement() {
+    world.enemies.forEach(enemy => {
+        enemy.startMotionX();
+        enemy.animate('walk');
+    });
+}
+
+function hideElement(elementId) {
+    document.getElementById(elementId).classList.add('d-none');
 }
 
 
@@ -31,14 +53,6 @@ async function loadStuff() {
     await world.loadEnemies();
     await world.loadBottleTemplate('./game/bottle.json');
     await world.loadEndboss('./game/polloLoco.json');
-}
-
-function startEnemyMovement() {
-    world.enemies.forEach(enemy => {
-        enemy.startMotionX();
-        enemy.animate('walk');
-
-    });
 }
 
 // let x= new Statusbar('healthbar');
