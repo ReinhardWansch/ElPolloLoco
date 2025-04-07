4ee80fba95c3a5c4f041029bb467141a01118932
MetalRick <lernen@reinitestet.at>
2025-04-07 10:25:43 +0200

    activate endboss

--- js/models/Endboss.class.js
+++ js/models/Endboss.class.js
@@ -1,7 +1,12 @@
 class Endboss extends LivingObject {
+    isActive= false;
+    actionLoop;
 
     activate() {
+        let alertDuration = this.getAnimationDuration('alert');
+        this.isActive = true;
         this.animate('alert');
+        window.setTimeout(this.startActionLoop.bind(this), alertDuration);
     }
 
     walk() {
@@ -14,5 +19,25 @@ class Endboss extends LivingObject {
         this.stopMotionX();
         this.animate('attack');
         //shoot chick
+    }
+
+    startActionLoop() {
+        let attackDuration = this.getAnimationDuration('attack');
+        this.actionLoop= window.setInterval(() => {
+            if (this.isActive) {
+                let randomNumer = Math.floor(Math.random() * 2);
+                if (randomNumer < 1) {
+                    this.walk();
+                } else {
+                    this.attack();
+                }
+            } else {
+                this.actionLoop = null;
+            }
+        }, attackDuration);
+    }
+
+    getAnimationDuration(animationName) {
+        return this.animationImages[animationName].imageDuration * this.animationImages[animationName].images.length;
     }
 }
